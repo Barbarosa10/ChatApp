393afd8222d6bd96d388e5bf89f4030ec4df421a
Barbarosa10 <duciucdanut10@gmail.com>
2023-05-22 17:43:42 +0300

    chat, contact, conversation

---
+++ ChatApp/Chat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    class Chat
+    {
+        private List<Conversation> _conversations = new List<Conversation>();
+        private List<Contact> _contacts = new List<Contact>();
+
+        public List<Conversation> Conversations
+        {
+            get { return _conversations; }
+        }
+        public List<Contact> Contacts
+        {
+            get { return _contacts; }
+        }
+        public void AddContact(String username, Bitmap image )
+        {
+            _contacts.Add(new Contact(username, image));
+        }
+        public void RemoveContact(String username)
+        {
+            foreach(Contact contact in _contacts)
+            {
+                if (contact.Name.Equals(username))
+                {
+                    _contacts.Remove(contact);
+                    break;
+                }
+            }
+        }
+        public void AddConversation(String timestamp, String username, String message)
+        {
+
+        }
+        public void RemoveConversation()
+        {
+
+        }
+    }
+}
---
+++ ChatApp/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    public class Contact
+    {
+        private String _name;
+        private Bitmap _image;
+
+        public Contact(String name, Bitmap image)
+        {
+            _name = name;
+            _image = image;
+        }
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Bitmap Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+}
